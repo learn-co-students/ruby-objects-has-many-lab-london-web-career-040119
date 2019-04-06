bdc12c5d1da9e202a6b00567a999ef2814b34bfa
Dwake5 <wakeling101@hotmail.com>
2019-04-06 12:27:06 +0100

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,28 @@
+class Artist
+
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def songs
+    Song.all.select { |song| song.artist == self}
+  end
+
+  def add_song(song_name)
+    song_name.artist = self
+  end
+
+  def add_song_by_name(song_name)
+   song = Song.new(song_name)
+   song.artist = self
+ end
+
+   def self.song_count
+     Song.all.count
+   end
+end
